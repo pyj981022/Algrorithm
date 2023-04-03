3c8066f15aaaf09b0307792de564b9abd4a14a4a
pyj981022 <106724939+pyj981022@users.noreply.github.com>
2023-04-03 11:03:05 +0900

    Update 피자 나눠 먹기 （1）.py
    주석 추가

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,7 +1,7 @@
 def solution(n):
     result = n%7
     result_a = n//7
-    if result == 0:
-        return result_a
-    else:
-        return result_a+1
+    if result == 0: # 7의 배수일 때
+        return result_a # 입력된 n을 7로 나눈 몫 출력
+    else: # 7의 배수가 아닐 때
+        return result_a+1 # 입력된 n을 7로 나눈 몫에서 1 더하기
