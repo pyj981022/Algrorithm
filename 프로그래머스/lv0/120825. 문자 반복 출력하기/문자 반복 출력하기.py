3a5bff98cad96481ebfa676d9dfdd0f1313fa952
pyj981022 <106724939+pyj981022@users.noreply.github.com>
2023-04-11 10:00:24 +0900

    Update 문자 반복 출력하기.py
    주석 추가

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,9 +1,14 @@
 def solution(my_string, n):
+    # 입력받은 my_string값 중 한 자씩 lst에 저장
     lst = []
+    # n번 곱한 값을 lst2에 저장
     lst2 = []
+    # my_string의 한 글자씩 lst에 저장
     for i in my_string:
         lst.append(i)
+    # 한 글자씩 n번 곱한 것을 lst2에 저장
     for a in lst:
         b = a*n
         lst2.append(b)
+    # 최종적으로 lst2의 원소들을 join해 
     return ''.join(lst2)
