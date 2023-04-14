52e354fec86a3485fe7fb4b2062bfb2d31767d69
pyj981022 <106724939+pyj981022@users.noreply.github.com>
2023-04-15 02:28:08 +0900

    Update 치킨 쿠폰.py
    주석 추가

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -2,7 +2,9 @@ def solution(chicken):
     answer = 0
     
     while chicken >= 10:
+        # 서비스 치킨 마리수
         free = chicken // 10
+        # 남은 쿠폰 수
         mod = chicken % 10
         answer += free
         chicken = free+mod
