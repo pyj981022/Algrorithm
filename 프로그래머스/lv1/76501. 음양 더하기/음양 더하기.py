d12724c0541f9f1163bb5e44ed5d24de11c80c26
pyj981022 <106724939+pyj981022@users.noreply.github.com>
2023-04-17 08:57:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,11 +1,3 @@
 def solution(absolutes, signs):
-    answer = 0
-    
-    for i in range(len(absolutes)):
-        # 양수일 때
-        if signs[i]:
-            answer += absolutes[i]
-        # 음수일 때
-        else:
-            answer -= absolutes[i]
+    answer = 123456789
     return answer
